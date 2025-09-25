3ccdc676bf03a97e4dd7a304c3c6eb62401e88fd
j-atkins <106238905+j-atkins@users.noreply.github.com>
2025-09-25 16:00:00 +0200

    remove zooplankton variable from CTD_BGC

--- src/virtualship/instruments/ctd_bgc.py
+++ src/virtualship/instruments/ctd_bgc.py
@@ -27,7 +27,6 @@ _CTD_BGCParticle = JITParticle.add_variables(
         Variable("po4", dtype=np.float32, initial=np.nan),
         Variable("ph", dtype=np.float32, initial=np.nan),
         Variable("phyc", dtype=np.float32, initial=np.nan),
-        Variable("zooc", dtype=np.float32, initial=np.nan),
         Variable("nppv", dtype=np.float32, initial=np.nan),
         Variable("raising", dtype=np.int8, initial=0.0),  # bool. 0 is False, 1 is True.
         Variable("max_depth", dtype=np.float32),
@@ -59,10 +58,6 @@ def _sample_ph(particle, fieldset, time):
 
 def _sample_phytoplankton(particle, fieldset, time):
     particle.phyc = fieldset.phyc[time, particle.depth, particle.lat, particle.lon]
-
-
-def _sample_zooplankton(particle, fieldset, time):
-    particle.zooc = fieldset.zooc[time, particle.depth, particle.lat, particle.lon]
 
 
 def _sample_primary_production(particle, fieldset, time):
@@ -166,7 +161,6 @@ def simulate_ctd_bgc(
             _sample_phosphate,
             _sample_ph,
             _sample_phytoplankton,
-            _sample_zooplankton,
             _sample_primary_production,
             _ctd_bgc_cast,
         ],
