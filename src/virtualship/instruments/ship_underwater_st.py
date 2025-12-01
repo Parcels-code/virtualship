c9cc9d76a04b16834f941a0e13dd4eb366145cff
Jamie Atkins <106238905+j-atkins@users.noreply.github.com>
2025-12-01 14:38:39 +0100

    Small fix for underway ST instrument (#257)
    * add small buffer to spacetime region, may help avoid some issues
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/virtualship/instruments/ship_underwater_st.py
+++ src/virtualship/instruments/ship_underwater_st.py
@@ -58,6 +58,10 @@ class Underwater_STInstrument(Instrument):
     def __init__(self, expedition, from_data):
         """Initialize Underwater_STInstrument."""
         variables = {"S": "so", "T": "thetao"}
+        spacetime_buffer_size = {
+            "latlon": 0.25,  # [degrees]
+            "time": 0.0,  # [days]
+        }
 
         super().__init__(
             expedition,
@@ -65,7 +69,7 @@ class Underwater_STInstrument(Instrument):
             add_bathymetry=False,
             allow_time_extrapolation=True,
             verbose_progress=False,
-            spacetime_buffer_size=None,
+            spacetime_buffer_size=spacetime_buffer_size,
             limit_spec=None,
             from_data=from_data,
         )
