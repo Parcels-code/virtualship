dc80617fbf5e071341bb3d3ad928ae4c4c0ba4b4
j-atkins <106238905+j-atkins@users.noreply.github.com>
2026-02-02 09:42:18 +0100

    scenario class tests

---
+++ tests/make_realistic/problems/test_scenarios.py
@@ -0,0 +1,57 @@
+from dataclasses import is_dataclass
+from datetime import timedelta
+
+from virtualship.instruments.types import InstrumentType
+from virtualship.make_realistic.problems.scenarios import (
+    GeneralProblem,
+    InstrumentProblem,
+)
+from virtualship.utils import GENERAL_PROBLEM_REG, INSTRUMENT_PROBLEM_REG
+
+
+def _assert_general_problem_class(cls):
+    assert issubclass(cls, GeneralProblem)
+    instance = cls()
+    assert is_dataclass(instance)
+
+    # required attributes and types
+    assert hasattr(instance, "message")
+    assert isinstance(instance.message, str)
+    assert instance.message.strip(), "message should not be empty"
+
+    assert hasattr(instance, "delay_duration")
+    assert isinstance(instance.delay_duration, timedelta)
+
+    assert hasattr(instance, "pre_departure")
+    assert isinstance(instance.pre_departure, bool)
+
+
+def _assert_instrument_problem_class(cls):
+    assert issubclass(cls, InstrumentProblem)
+    instance = cls()
+    assert is_dataclass(instance)
+
+    # required attributes and types
+    assert hasattr(instance, "message")
+    assert isinstance(instance.message, str)
+    assert instance.message.strip(), "message should not be empty"
+
+    assert hasattr(instance, "delay_duration")
+    assert isinstance(instance.delay_duration, timedelta)
+
+    assert hasattr(instance, "instrument_type")
+    assert isinstance(instance.instrument_type, InstrumentType)
+
+
+def test_general_problems():
+    assert GENERAL_PROBLEM_REG, "GENERAL_PROBLEM_REG should not be empty"
+
+    for cls in GENERAL_PROBLEM_REG:
+        _assert_general_problem_class(cls)
+
+
+def test_instrument_problems():
+    assert INSTRUMENT_PROBLEM_REG, "INSTRUMENT_PROBLEM_REG should not be empty"
+
+    for cls in INSTRUMENT_PROBLEM_REG:
+        _assert_instrument_problem_class(cls)
