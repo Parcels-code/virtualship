ba585c2a537fe447e924735100594c20fd3528bf
j-atkins <106238905+j-atkins@users.noreply.github.com>
2026-03-25 13:31:32 +0100

    remove CTD_BGC instrument type from InstrumentType enum, add SensorType enum

--- src/virtualship/instruments/types.py
+++ src/virtualship/instruments/types.py
@@ -5,7 +5,6 @@ class InstrumentType(Enum):
     """Types of the instruments."""
 
     CTD = "CTD"
-    CTD_BGC = "CTD_BGC"
     DRIFTER = "DRIFTER"
     ARGO_FLOAT = "ARGO_FLOAT"
     XBT = "XBT"
@@ -16,3 +15,17 @@ class InstrumentType(Enum):
     def is_underway(self) -> bool:
         """Return True if instrument is an underway instrument (ADCP, UNDERWATER_ST)."""
         return self in {InstrumentType.ADCP, InstrumentType.UNDERWATER_ST}
+
+
+class SensorType(str, Enum):
+    """Sensors available (to instruments with configurable sensors, e.g. CTDs). #TODO: and soon Argo floats, drifters."""
+
+    TEMPERATURE = "TEMPERATURE"
+    SALINITY = "SALINITY"
+    OXYGEN = "OXYGEN"
+    CHLOROPHYLL = "CHLOROPHYLL"
+    NITRATE = "NITRATE"
+    PHOSPHATE = "PHOSPHATE"
+    PH = "PH"
+    PHYTOPLANKTON = "PHYTOPLANKTON"
+    PRIMARY_PRODUCTION = "PRIMARY_PRODUCTION"
