2c32b9c2c029b555040f561a41c85bda5d519c8a
j-atkins <106238905+j-atkins@users.noreply.github.com>
2026-07-29 14:38:19 +0200

    migrate UnderwaterST instrument to new underway instrument logic

--- src/virtualship/instruments/ship_underwater_st.py
+++ src/virtualship/instruments/ship_underwater_st.py
@@ -1,51 +1,40 @@
 from collections.abc import Callable
-from dataclasses import dataclass
 from typing import ClassVar
 
 import numpy as np
-from parcels import ParticleFile, ParticleSet
+import parcels
 
-from virtualship.instruments.base import FetchSpec, Instrument
+from virtualship.instruments.base import (
+    FetchSpec,
+    UnderwayCoordinates,
+    UnderwayInstrument,
+)
 from virtualship.instruments.sensors import SensorType
 from virtualship.instruments.types import InstrumentType
 from virtualship.utils import (
-    build_particle_class_from_sensors,
     register_instrument,
 )
-
-# =====================================================
-# SECTION: Dataclass
-# =====================================================
-
-
-@dataclass
-class Underwater_ST:
-    """Underwater_ST configuration."""
-
-    name: ClassVar[str] = "Underwater_ST"
-
-
-# =====================================================
-# SECTION: non-sensor Particle Variables (non-sampling)
-# =====================================================
-
-# Underwater ST has no non-sensor variables, only sensor variables.
-_ST_NONSENSOR_VARIABLES: list = []
-
 
 # =====================================================
 # SECTION: Kernels
 # =====================================================
 
-
-# define function sampling Salinity
-def _sample_salinity(particles, fieldset):
-    particles.S = fieldset.S[particles.t, particles.z, particles.y, particles.x]
+# N.B. underway 'kernels' are special cases, where the particleset is not needed, and the kernel is not passed to `pset.execute()` as would be done for a typical Parcels workflow.
+# Instead, the 'kernel' function is used only once to evaluate the fieldset at given times, depths, lats, lons.
 
 
-# define function sampling Temperature
-def _sample_temperature(particles, fieldset):
-    particles.T = fieldset.T[particles.t, particles.z, particles.y, particles.x]
+def _sample_underway_salinity(fieldset: parcels.FieldSet, coords: UnderwayCoordinates):
+    return fieldset.S.eval(
+        t=coords.times, z=coords.depths, x=coords.lons, y=coords.lats
+    )
+
+
+def _sample_underway_temperature(
+    fieldset: parcels.FieldSet, coords: UnderwayCoordinates
+):
+    return fieldset.T.eval(
+        t=coords.times, z=coords.depths, x=coords.lons, y=coords.lats
+    )
 
 
 # =====================================================
@@ -54,12 +43,12 @@ def _sample_temperature(particles, fieldset):
 
 
 @register_instrument(InstrumentType.UNDERWATER_ST)
-class Underwater_STInstrument(Instrument):
+class Underwater_STInstrument(UnderwayInstrument):
     """Underwater_ST instrument class."""
 
     sensor_kernels: ClassVar[dict[SensorType, Callable]] = {
-        SensorType.TEMPERATURE: _sample_temperature,
-        SensorType.SALINITY: _sample_salinity,
+        SensorType.TEMPERATURE: _sample_underway_temperature,
+        SensorType.SALINITY: _sample_underway_salinity,
     }
 
     def __init__(self, expedition, from_data):
@@ -80,49 +69,41 @@ class Underwater_STInstrument(Instrument):
 
     def simulate(self, measurements, out_path) -> None:
         """Simulate underway salinity and temperature measurements."""
+        st_config = self.expedition.instruments_config.ship_underwater_st_config
+
         DEPTH = -2.0
 
         measurements.sort(key=lambda p: p.time)
 
         fieldset = self.load_input_data()
 
-        # build dynamic particle class from the active sensors
-        st_config = self.expedition.instruments_config.ship_underwater_st_config
-        _ShipSTParticle = build_particle_class_from_sensors(
-            st_config.sensors, _ST_NONSENSOR_VARIABLES
+        # use first active field for time reference
+        _time_ref_key = next(iter(self.variables))
+        _time_ref_field = getattr(fieldset, _time_ref_key)
+        fieldset_starttime = _time_ref_field.data.time.isel(time=0).values
+
+        # sampling times and locations
+        times = np.array(
+            [
+                (np.datetime64(point.time) - fieldset_starttime)
+                / np.timedelta64(1, "s")
+                for point in measurements
+            ]
+        )
+        lons = np.array([point.location.lon for point in measurements])
+        lats = np.array([point.location.lat for point in measurements])
+        coords = UnderwayCoordinates(
+            times, lons, lats, depths=np.full_like(times, DEPTH)
         )
 
-        particleset = ParticleSet(
-            fieldset=fieldset,
-            pclass=_ShipSTParticle,
-            x=0.0,
-            y=0.0,
-            z=DEPTH,
+        sampled = self._sample_underway(
+            config_sensors=st_config.sensors, fieldset=fieldset, coords=coords
         )
 
-        out_file = ParticleFile(path=out_path, outputdt=np.inf)
-
-        # build kernel list from active sensors only
-        sampling_kernels = [
-            self.sensor_kernels[sc.sensor_type]
-            for sc in st_config.sensors
-            if sc.enabled and sc.sensor_type in self.sensor_kernels
-        ]
-
-        # TODO: need to overhaul UNDERWATER_ST/underway instruments generally... don't think this Parcels API works anymore
-        # TODO: a good time to implement https://github.com/Parcels-code/virtualship/issues/231
-
-        for point in measurements:
-            particleset.lon_nextloop[:] = point.location.lon
-            particleset.lat_nextloop[:] = point.location.lat
-            particleset.time_nextloop[:] = fieldset.time_origin.reltime(
-                np.datetime64(point.time)
-            )
-
-            particleset.execute(
-                sampling_kernels,
-                dt=1,
-                runtime=1,
-                verbose_progress=self.verbose_progress,
-                output_file=out_file,
-            )
+        self._to_parquet(
+            dat_arrays=sampled,
+            var_names=self.variables.keys(),
+            fieldset_time_origin=fieldset_starttime,
+            out_path=out_path,
+            coords=coords,
+        )
